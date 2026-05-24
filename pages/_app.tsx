37318201c1d24a8599d6382714258a0dcbecf9ff
maruqyanhayk <maruqyan@selfstudio.se>
2026-05-24 21:41:03 +0400

    new description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from "next/app";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "@/app/globals.css";
+
+const GA_MEASUREMENT_ID = "G-MRSCWTHWR8";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,10 +17,24 @@ const geistMono = Geist_Mono({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} min-h-full antialiased`}
-    >
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+      <div
+        className={`${geistSans.variable} ${geistMono.variable} min-h-full antialiased`}
+      >
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
